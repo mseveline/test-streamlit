de0793ab47b3d22aee21bf2eec6d45a895b658fa
mseveline <146049660+mseveline@users.noreply.github.com>
2023-10-09 12:01:16 +0800

    tanpa desimal

--- uber_pickups.py
+++ uber_pickups.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.title('Kalkulator Sederhana')
 
 # Input field untuk memasukkan angka pertama
-angka1 = st.number_input('Masukkan Angka Pertama')
+angka1 = st.number_input('Masukkan Angka Pertama', format="%d")
 
 # Input field untuk memasukkan angka kedua
-angka2 = st.number_input('Masukkan Angka Kedua')
+angka2 = st.number_input('Masukkan Angka Kedua', format="%d")
 
 # Tombol untuk menghitung hasil pertambahan
 if st.button('Hitung'):
-    hasil = angka1 + angka2
+    hasil = int(angka1) + int(angka2)
     st.write(f'Hasil Pertambahan: {hasil}')
