a7a9450b3ba768d3aa3855c153b0e8b79445a2a2
mseveline <146049660+mseveline@users.noreply.github.com>
2023-10-09 11:57:19 +0800

    change code 1

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,33 +1,14 @@
-def tambahkan():
-    try:
-        angka1 = float(entry_angka1.get())
-        angka2 = float(entry_angka2.get())
-        hasil = angka1 + angka2
-        label_hasil.config(text=f'Hasil: {hasil}')
-    except ValueError:
-        label_hasil.config(text='Masukkan angka yang valid')
+import streamlit as st
 
-# Membuat jendela utama
-root = tk.Tk()
-root.title('Kalkulator Sederhana')
+st.title('Kalkulator Sederhana')
 
-# Membuat input field dan label
-label_angka1 = tk.Label(root, text='Angka 1:')
-label_angka1.pack()
-entry_angka1 = tk.Entry(root)
-entry_angka1.pack()
+# Input field untuk memasukkan angka pertama
+angka1 = st.number_input('Masukkan Angka Pertama')
 
-label_angka2 = tk.Label(root, text='Angka 2:')
-label_angka2.pack()
-entry_angka2 = tk.Entry(root)
-entry_angka2.pack()
+# Input field untuk memasukkan angka kedua
+angka2 = st.number_input('Masukkan Angka Kedua')
 
-# Membuat tombol untuk menghitung hasil
-tombol_hitung = tk.Button(root, text='Hitung', command=tambahkan)
-tombol_hitung.pack()
-
-# Membuat label untuk menampilkan hasil
-label_hasil = tk.Label(root, text='')
-label_hasil.pack()
-
-root.mainloop()
+# Tombol untuk menghitung hasil pertambahan
+if st.button('Hitung'):
+    hasil = angka1 + angka2
+    st.write(f'Hasil Pertambahan: {hasil}')
