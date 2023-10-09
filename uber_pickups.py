9e33e7510a70681816d7340ba59721f56b1256a1
mseveline <146049660+mseveline@users.noreply.github.com>
2023-10-09 12:13:20 +0800

    change 2

--- uber_pickups.py
+++ uber_pickups.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.title('Kalkulator Sederhana')
 
 # Input field untuk memasukkan angka pertama
-angka1 = st.number_input('Masukkan Angka Pertama', format="%d")
+angka1 = st.number_input('Masukkan Angka Pertama', format="%f")
 
 # Input field untuk memasukkan angka kedua
-angka2 = st.number_input('Masukkan Angka Kedua', format="%d")
+angka2 = st.number_input('Masukkan Angka Kedua', format="%f")
 
 # Tombol untuk menghitung hasil pertambahan
 if st.button('Hitung'):
-    hasil = int(angka1) + int(angka2)
+    hasil = int(angka1 + angka2)  # Mengonversi hasil ke bilangan bulat
     st.write(f'Hasil Pertambahan: {hasil}')
